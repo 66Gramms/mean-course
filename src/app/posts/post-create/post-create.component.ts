88cae13bda1131a8e82be993eb9276b9a0232cdb
66Gramms <meszarosricsi01@gmail.com>
2024-02-28 13:45:44 +0100

    add material to post-create.component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatInputModule, MatCardModule, MatButtonModule],
 })
 export class PostCreateComponent {
   enteredValue = 'asd';
